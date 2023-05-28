f97a7ecf02c44c50267f819cf027f52987fe7abf
= <=>
2023-05-28 18:38:30 -0300

    commit

--- src/public/js/swiper.js
+++ src/public/js/swiper.js
@@ -10,17 +10,27 @@ let conteinerDePlayers = document.getElementById('swiper-wrapper');
 let players = [];
 
 async function RunPlayerTwitch() {
-  const url = "https://api.twitch.tv/helix/streams?first=5&sort=viewers&language=pt";
+  const url = "https://api.twitch.tv/helix/streams?sort=viewers&language=pt";
   
   const response = await fetch(url, options);
   const data = await response.json();
+
   if (data && Array.isArray(data['data'])) {
-    for (let i = 0; i < data['data'].length; i++) {
-      let nameUser = data['data'][i].user_name;
+    //Filtrar apenas streamers com com not mature
+    let arrayNotMature = data['data'].filter((element)=>{
+      return element.is_mature === false
+    });
+
+
+   let arrayDeStreamers = arrayNotMature.slice(0, 5);
+    
+    arrayDeStreamers.forEach((elemento, indice)=>{
+      let nameUser = elemento.user_name
       players.push(nameUser)
-      conteinerDePlayers.innerHTML += `
-        <div data-hash="slide${i + 1}" class="swiper-slide">
-          <div id="twitch-embed${i + 1}" class="conteinerPlayer">
+      conteinerDePlayers.innerHTML +=
+       `
+        <div data-hash="slide${indice + 1}" class="swiper-slide">
+          <div id="twitch-embed${indice + 1}" class="conteinerPlayer">
             <div class="playerDescri">
               <div class="conteiner-imgPlayer">
                 <img src="" alt="">
@@ -31,34 +41,52 @@ async function RunPlayerTwitch() {
           </div>
         </div>
       `
-    }
+    });
+
+
+
   }
 }
+
+
 
 class ControllerPlayer {
 
   constructor () {
     this.player1 = null;
     this.player2 = null;
+    this.player3 = null;
   }
 
 
   PlayerTwitch1 (options) {
    this.player1 =  new Twitch.Player(`twitch-embed1`, options);
-   
+  
+
+   return this.player1
   }
 
   PlayerTwitch2 (options) {
    this.player2  =   new Twitch.Player(`twitch-embed2`, options);    
+
+  
+   return this.player2;
   }
 
+  PlayerTwitch3 (options){
+    this.player3  =   new Twitch.Player(`twitch-embed3`, options);    
+ 
+    return this.player3;
+  }
+
+  PauseTwtch (num) {
+    if(num == 1){
+      this.player2.pause()
+      this.player3.pause()
+    }
+  }
 
 }
-
-
-
-
-
 
 
 
@@ -68,14 +96,12 @@ let controllerPlayer = new ControllerPlayer();
 
 async function init() {
   await RunPlayerTwitch();
-  console.log(players);
- 
 
-  controllerPlayer.PlayerTwitch1({width:800, height:450, channel:players[0]})
-  controllerPlayer.PlayerTwitch2({width:800, height:450, channel:players[1]})  
+    controllerPlayer.PlayerTwitch1({width:450, height:850, channel:players[0]});
+    controllerPlayer.PlayerTwitch2({width:450, height:850, channel:players[1]});
+    controllerPlayer.PlayerTwitch3({width:450, height:850, channel:players[3]});
 
-
-  let swiper = new Swiper(".mySwiper", {
+    new Swiper(".mySwiper", {
 
      hashNavigation: {
        watchState: true,
@@ -89,35 +115,26 @@ async function init() {
        prevEl: ".swiper-button-prev",
      },
    
+
+
      on:{
       init: function () {
         console.log('Está no Slide 1');
-        if(controllerPlayer.player2){
-          controllerPlayer.player2.pause();
-
-        }
+     
+        controllerPlayer.PauseTwtch(1)
       },
 
        slideChange: function (){
-
          let activeSlideIndex = this.activeIndex;
-         console.log(activeSlideIndex+1);
-
-         switch (activeSlideIndex+1){
+         
+         
+         switch (activeSlideIndex + 1){
              case 1:
                 console.log('Está no Slide 1');
-
-                if(controllerPlayer.player2){
-                    controllerPlayer.player2.pause();
-                }
+                controllerPlayer.PauseTwtch(1)
                break;
              case 2:
-               console.log('Está no Slide 2');
-
-               if(controllerPlayer.player2){
-                controllerPlayer.player1.pause();
-               }
-
+               console.log('Está no Slide 2');          
                break
               case 3:
                 console.log('Está no Slide 3');

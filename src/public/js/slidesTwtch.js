c11415c07204bbf20cacc65524ef7fcaa6e709be
= <=>
2023-05-29 13:01:57 -0300

    commit

--- src/public/js/slidesTwtch.js
+++ src/public/js/slidesTwtch.js
@@ -7,13 +7,18 @@ const options = {
 
 
 let conteinerDePlayers = document.getElementById('swiper-wrapper');
-let players = [];
+
+let players     = [];
+let nameGame    = [];
+let descriGame  = [];
+let picture     = [];
 
 async function RunPlayerTwitch() {
   const url = "https://api.twitch.tv/helix/streams?sort=viewers&language=pt";
   
   const response = await fetch(url, options);
   const data = await response.json();
+
 
   if (data && Array.isArray(data['data'])) {
     //Filtrar apenas streamers com com not mature
@@ -23,30 +28,55 @@ async function RunPlayerTwitch() {
 
 
    let arrayDeStreamers = arrayNotMature.slice(0, 5);
-    
-    arrayDeStreamers.forEach((elemento, indice)=>{
-      let nameUser = elemento.user_name
-      players.push(nameUser)
-      conteinerDePlayers.innerHTML +=
-       `
-        <div data-hash="slide${indice + 1}" class="swiper-slide">
-          <div id="twitch-embed${indice + 1}" class="conteinerPlayer">
-            <div class="playerDescri">
-              <div class="conteiner-imgPlayer">
-                <img src="" alt="">
-              </div>
-              <div class="nameStreamerPlayer"> ${nameUser}</div>
-              <div class="descriStreamerPlayer"></div>
-            </div>
-          </div>
-        </div>
-      `
+   
+
+
+    arrayDeStreamers.forEach((elemento)=>{
+      let nameUser = elemento.user_name;
+      let name_game = elemento.game_name;
+      players.push(nameUser);
+      nameGame.push(name_game);
     });
 
-
+    
 
   }
+
+ for(let dados of players){
+  const response =  await fetch(`https://api.twitch.tv/helix/users?login=${dados}`, options);
+  const data = await response.json();
+  descriGame.push(data['data'][0].description);
+  picture.push(data['data'][0].profile_image_url)
+ 
+ }
+
+
+
+ for(let i = 0; i < 4; i++){
+  conteinerDePlayers.innerHTML += 
+  `
+  <div data-hash="slide${i + 1}" class="swiper-slide">
+    <div id="twitch-embed${i + 1}" class="conteinerPlayer">
+      <div class="playerDescri">
+        <div class="conteiner-imgPlayer">
+          <img src="${picture[i]}" alt="picture-streamer">
+        </div>
+        <div class="descriStreamerPlayer">${descriGame[i]}</div>
+        <div class="nameGamePlayer"> ${nameGame[i]}</div>
+        <div class="nameStreamerPlayer">${players[i]}</div>
+      </div>
+    </div>
+  </div>
+`
+
+ }
+
+ console.log(players);
+ console.log(nameGame);
+ console.log(descriGame);
+ console.log(picture);
 }
+
 
 
 
@@ -71,6 +101,9 @@ class ControllerPlayer {
 
   PlayerTwitch1 (options) {
     this.player1 = new Twitch.Player(`twitch-embed1`, options);
+    setTimeout(() => {
+      this.player1.setVolume(0.5); // Definir volume para 0.5 (50%)
+    }, 2000); 
   }
 
   PlayerTwitch2 (options) {
@@ -92,48 +125,48 @@ class ControllerPlayer {
 
   PauseTwtch (num) {
     if(num == 1){
-      this.player2 ? this.player2.pause() : '';
-      this.player3 ? this.player3.pause() : '';
-      this.player4 ? this.player4.pause() : '';
-      this.player5 ? this.player5.pause() : '';
+      this.player2 ? this.player2.pause() : void 0;
+      this.player3 ? this.player3.pause() : void 0;
+      this.player4 ? this.player4.pause() : void 0;
+      this.player5 ? this.player5.pause() : void 0;
     
      this.player1.play();
     }
 
     if(num == 2){
-      this.player1 ? this.player1.pause() : '';
-      this.player3 ? this.player3.pause() : '';
-      this.player4 ? this.player4.pause() : '';
-      this.player5 ? this.player5.pause() : '';
+      this.player1 ? this.player1.pause() : void 0;
+      this.player3 ? this.player3.pause() : void 0;
+      this.player4 ? this.player4.pause() : void 0;
+      this.player5 ? this.player5.pause() : void 0;
 
       this.player2.play();
     }
 
     if(num == 3){
 
-      this.player1 ? this.player1.pause(): '';
-      this.player2 ? this.player2.pause(): '';
-      this.player4 ? this.player4.pause(): '';
-      this.player5 ? this.player5.pause(): '';
+      this.player1 ? this.player1.pause(): void 0;
+      this.player2 ? this.player2.pause(): void 0;
+      this.player4 ? this.player4.pause(): void 0;
+      this.player5 ? this.player5.pause(): void 0;
 
       this.player3.play();
     }
 
     if(num == 4){
-      this.player1 ? this.player1.pause() : '';
-      this.player2 ? this.player2.pause() : '';
-      this.player3 ? this.player3.pause() : '';
-      this.player5 ? this.player5.pause() : '';
+      this.player1 ? this.player1.pause() : void 0;
+      this.player2 ? this.player2.pause() : void 0;
+      this.player3 ? this.player3.pause() : void 0;
+      this.player5 ? this.player5.pause() : void 0;
 
       this.player4.play();
     }
 
 
     if(num == 5){
-      this.player1 ? this.player1.pause() : '';
-      this.player2 ? this.player2.pause() : '';
-      this.player3 ? this.player3.pause() : '';
-      this.player4 ? this.player4.pause() : '';
+      this.player1 ? this.player1.pause() : void 0;
+      this.player2 ? this.player2.pause() : void 0;
+      this.player3 ? this.player3.pause() : void 0;
+      this.player4 ? this.player4.pause() : void 0;
 
       this.player5.play();
     }
@@ -147,13 +180,7 @@ let controllerPlayer = new ControllerPlayer();
 
 async function init() {
   await RunPlayerTwitch();
-
-  if(controllerPlayer.createPlayerTwitch1 == false){
-    controllerPlayer.PlayerTwitch1({width:450, height:850, channel:players[0]});
-    controllerPlayer.createPlayerTwitch1 = true;
-  }
-
-
+  
   new Swiper(".mySwiper", {
 
      hashNavigation: {
@@ -175,6 +202,12 @@ async function init() {
      on:{
       init: function () {
         console.log('Está no Slide 1');
+        if(controllerPlayer.createPlayerTwitch1 == false){
+          controllerPlayer.PlayerTwitch1({width:450, height:850, channel:players[0]});
+          controllerPlayer.createPlayerTwitch1 = true;
+        }
+
+        
       },
 
        slideChange: function (){

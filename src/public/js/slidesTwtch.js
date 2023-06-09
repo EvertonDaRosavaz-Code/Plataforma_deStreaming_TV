74e7add4a6c3ca90b77e56d7ae5f7bce683e9187
Everton Da Rosa Vaz <111449550+EvertonDaRosavaz-Code@users.noreply.github.com>
2023-06-09 07:13:54 -0300

    Update slidesTwtch.js

--- src/public/js/slidesTwtch.js
+++ src/public/js/slidesTwtch.js
@@ -1,7 +1,7 @@
 const options = {
   headers : {
-    Authorization: `Bearer pqlbi0btqoxj9qrqo09f8k9qex7en4`,
-    'Client-Id': `mo9ref2fe7vxqzhd9f35fmo1uor3jv`
+    Authorization: `Bearer `,
+    'Client-Id': ``
   }
 }
 
